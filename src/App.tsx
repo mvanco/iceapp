274164b459321ebae441dc22b5116ee70a80a99d
Matus Vanco <matus.vanco@24i.com>
2024-05-09 16:38:00 +0200

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,89 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+
+function onLogout() {
+
+}
+
+function onRefresh() {
+
+}
+
+function onWeb() {
+
+}
+
+const LoginScreen = () => {
+  return (
+    <>
+      <h2>Login screen</h2>
+      <Link to="/internal">Log in</Link>
+    </>
+  )
+}
+
+const InternalScreen = () => {
+  return (
+    <>
+      <h2>Internal page</h2>
+      <div className="Menu">
+        <Link to="/internal/home">Home</Link>
+        <Link to="/internal/terms">Terms</Link>
+      </div>
+    </>
+  )
+}
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h2>Home screen</h2>
+      <div className="Menu">
+        <Link to="/internal/home">Home</Link>
+        <Link to="/internal/terms">Terms</Link>
+      </div>
+    </>
+  )
+}
+
+const TermsScreen = () => {
+  return (
+    <>
+      <h2>Terms screen</h2>
+      <div className="Menu">
+        <Link to="/internal/home">Home</Link>
+        <Link to="/internal/terms">Terms</Link>
+      </div>
+    </>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <div className="AppBar">
+            <a href="#" onClick={onLogout}>Logout</a>&nbsp;
+            <a href="#" onClick={onRefresh}>Refresh</a>&nbsp;
+            <a href="#" onClick={onWeb}>Web App</a>
+          </div>
+          <div className="Page">
+            <Routes>
+              <Route path="/" element={<LoginScreen />} />
+              <Route path="/internal/*">
+                <Route index element={<HomeScreen />} />
+                <Route path="home" element={<HomeScreen />} />
+                <Route path="terms" element={<TermsScreen />} />
+              </Route>
+            </Routes>
+          </div>
+        </div>
+      </Router>
     </div>
   );
 }
