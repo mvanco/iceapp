f6d689bc83d7836745d16f8de47b632351dee939
Matus Vanco <matus.vanco@24i.com>
2024-08-14 23:48:10 +0200

    Handle rentals in repo

--- src/repo/AdminActionsRepo.tsx
+++ src/repo/AdminActionsRepo.tsx
@@ -88,6 +88,169 @@ class AdminActionsRepo {
       return {error: ChangeCreditErrorEnum.Unknown};
     }
   }
+
+  static async rentals(): Promise<Rentals.Response[] | Rentals.Error> {
+    if (CurrentConfig.token === undefined) {
+      return {error: Rentals.ErrorEnum.Unknown};
+    }
+    try {
+      const bodyData = new URLSearchParams();
+      bodyData.append("token", CurrentConfig.token);
+      const response = await fetch(`${CurrentConfig.RestApiUrl}/rentals`, {
+        method: "POST",
+        body: bodyData
+      });
+      if (!response.ok) {
+        return {error: Rentals.ErrorEnum.Unknown};
+      }
+      const data = await response.json();
+
+      if (data.error !== undefined) {
+        for (const value of Object.values(Rentals.ErrorEnum)) {
+          if (data.error === value) {
+            return {error: value};;
+          }
+        }
+        return {error: Rentals.ErrorEnum.Unknown};
+      }
+
+      return data.rentals;
+    } catch(error) {
+      console.error('Error fetching data:', error);
+      return {error: Rentals.ErrorEnum.Unknown};
+    }
+  }
+
+  static async deleteRental(id: number) {
+    if (CurrentConfig.token === undefined) {
+      return {error: DeleteRental.ErrorEnum.Unknown};
+    }
+    try {
+      const bodyData = new URLSearchParams();
+      bodyData.append("token", CurrentConfig.token);
+      bodyData.append("rental_id", id.toString());
+      const response = await fetch(`${CurrentConfig.RestApiUrl}/delete_rental`, {
+        method: "POST",
+        body: bodyData
+      });
+      if (!response.ok) {
+        return {error: DeleteRental.ErrorEnum.Unknown};
+      }
+      const data = await response.json();
+
+      if (data.error !== undefined) {
+        for (const value of Object.values(DeleteRental.ErrorEnum)) {
+          if (data.error === value) {
+            return {error: value};;
+          }
+        }
+        return {error: DeleteRental.ErrorEnum.Unknown};
+      }
+
+      return {};
+    } catch(error) {
+      console.error('Error fetching data:', error);
+      return {error: DeleteRental.ErrorEnum.Unknown};
+    }
+  }
+
+
+  /**
+   * Add new rental to database for currently logged-in admin.
+   * @param start Start date and time of rental using format e.g. 2024-11-22T11:11
+   * @param duration Rental duration in minutes
+   * @param price Price in CZK or EUR
+   * @param minCapacity Minimum capacity for rental
+   * @param maxCapacity Maximum capacity for rental
+   * @returns nothing
+   */
+  static async addRental(
+    start: string,
+    duration: number,
+    price: number,
+    minCapacity: number,
+    maxCapacity: number
+  ) {
+    if (CurrentConfig.token === undefined) {
+      return {error: AddRental.ErrorEnum.Unknown};
+    }
+    try {
+      const bodyData = new URLSearchParams();
+      bodyData.append("token", CurrentConfig.token);
+      bodyData.append("start", start);
+      bodyData.append("duration", duration.toString());
+      bodyData.append("price", price.toString());
+      bodyData.append("min_capacity", minCapacity.toString());
+      bodyData.append("max_capacity", maxCapacity.toString());
+      const response = await fetch(`${CurrentConfig.RestApiUrl}/add_rental`, {
+        method: "POST",
+        body: bodyData
+      });
+      if (!response.ok) {
+        return {error: AddRental.ErrorEnum.Unknown};
+      }
+      const data = await response.json();
+
+      if (data.error !== undefined) {
+        for (const value of Object.values(AddRental.ErrorEnum)) {
+          if (data.error === value) {
+            return {error: value};;
+          }
+        }
+        return {error: AddRental.ErrorEnum.Unknown};
+      }
+
+      return {};
+    } catch(error) {
+      console.error('Error fetching data:', error);
+      return {error: AddRental.ErrorEnum.Unknown};
+    }
+  }
+}
+
+export namespace Rentals {
+  export interface Response {
+    id: number,
+    start: string,
+    duration: number,
+    price: number,
+    min_capacity: number,
+    max_capacity: number,
+    registered: boolean
+    confirmed: boolean
+  }
+
+  export enum ErrorEnum {
+    Unknown = "unknown"
+  }
+
+  export interface Error {
+    error: ErrorEnum
+  }
+}
+
+export namespace DeleteRental {
+  export interface Response {}
+
+  export enum ErrorEnum {
+     Unknown = "unknown"
+  }
+
+  export interface Error {
+    error: ErrorEnum
+  }
+}
+
+export namespace AddRental {
+  export interface Response {}
+
+  export enum ErrorEnum {
+     Unknown = "unknown"
+  }
+
+  export interface Error {
+    error: ErrorEnum
+  }
 }
 
 export {
